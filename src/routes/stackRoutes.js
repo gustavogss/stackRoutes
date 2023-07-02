db064793745fb03ec22a318f4b53f464987cce31
gustavogss <gustavogss.dev@gmail.com>
2023-07-02 00:21:29 -0300

    Implementando Rotas de Navegação

---
+++ src/routes/stackRoutes.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export function StackRoutes() {
+  return (
+    <View>
+      <Text>StackRoutes</Text>
+    </View>
+  )
+}
